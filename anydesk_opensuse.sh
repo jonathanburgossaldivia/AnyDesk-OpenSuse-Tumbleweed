a9bee53ff530541acbf8d5c83e925a1852ec4032
Jonathan Burgos Saldivia <jonathanburgoss@outlook.com>
2021-06-17 12:18:06 -0400

    Add files via upload

---
+++ anydesk_opensuse.sh
@@ -0,0 +1,5 @@
+#install dependencies
+sudo rpm -ivh  https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.2/repo/oss/x86_64/pangox-compat-0.0.2-lp152.4.2.x86_64.rpm
+sudo rpm -ivh  https://ftp.lysator.liu.se/pub/opensuse/distribution/leap/15.2/repo/oss/x86_64/libpangox-1_0-0-0.0.2-lp152.4.2.x86_64.rpm
+#install AnyDesk
+sudo rpm -ivh  https://download.anydesk.com/linux/anydesk_6.1.1-1_x86_64.rpm
